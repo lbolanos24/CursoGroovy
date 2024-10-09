9fbc9587672ba43a43c2e324e0a235c500d52cf6
lpbolanos24 <lpbolanos@hotmail.com>
2024-10-09 16:18:17 -0500

    Ejercicios de la seccion2

---
+++ Scripts/Person.groovy
@@ -0,0 +1,13 @@
+class Person {
+	String first
+	String last
+	
+	public Person(String first, String last){
+		this.first=first
+		this.last=last
+	}
+	
+	public String toString(){
+		"Person: $first $last"
+	}
+}
